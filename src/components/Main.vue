80e31841c5b146621dd3eb1e87cd75b8f134d854
adit.it <adit.it@langitpayment.com>
2022-06-13 11:10:33 +0700

    adding subtitle

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="bg-white dark:bg-gray-500 text-black dark:text-white p-5 flex items-center justify-center">
         <div class="w-80">
-            <lottie-player v-if="!dark" src="https://assets8.lottiefiles.com/datafiles/EkoZpNsbNMU956i/data.json"  background="transparent"  speed="1"  loop  autoplay></lottie-player>
-            <lottie-player v-if="dark" src="https://assets7.lottiefiles.com/packages/lf20_3eq0grut.json"  background="rgba(107, 114, 128)"  speed="1"  loop  autoplay></lottie-player>
+            <lottie-player v-if="!dark" src="https://assets2.lottiefiles.com/packages/lf20_M9p23l.json"  background="transparent"  speed="1"  loop  autoplay></lottie-player>
+            <lottie-player v-if="dark" src="https://assets2.lottiefiles.com/packages/lf20_M9p23l.json"  background="rgba(107, 114, 128)"  speed="1"  loop  autoplay></lottie-player>
         </div>
     </div>
 </template>
